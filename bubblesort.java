0435716bc87a4e7c118c5ab1c12c513552b4afa3
curious-companion <105918194+curious-companion@users.noreply.github.com>
2024-01-14 17:41:51 +0530

    Initial commit

---
+++ bubblesort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class bubblesort {
+
+    public static void bubble(int arr[]){
+        for(int turn = 0; turn < arr.length-1; turn++){
+            for(int j = 0; j < arr.length-1-turn; j++){
+                if(arr[j] < arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String args[]){
+        int arr1[] = {5,4,1,3,2};
+        bubble(arr1);
+        printArr(arr1);
+
+    }
+}
