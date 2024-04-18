ff96ddca3ed3d0157fe2292665b64e6fcdeb6a4d
Vibhanshu Jain <105918194+curious-companion@users.noreply.github.com>
2024-04-18 11:59:45 +0530

    countingsort.java

--- countingsort.java
+++ countingsort.java
@@ -5,6 +5,15 @@ public class countingsort {
             largest = Math.max(largest,arr[i])
         }
         int count[] == new int[largest+1];
-        
+        for(int i = 0; i<arr.length; i++){
+            count[arr[i]]++;        
     }
+        int j = 0;
+        for(int i = 0; i<count.length; i++){
+            while(count[i] > 0){
+                arr[j]=i;
+                j++;
+                count[i}--;
+            }
+        }
 }
